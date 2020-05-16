6629d3b911e7cc0ad9c1244921cc2f240fe244de
royleochan <royleochan@gmail.com>
2020-05-16 16:54:27 +0800

    Creating screens

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const FavoritesScreen = () => {
+    return (
+        <View style={styles.screen}>
+            <Text>The Categories Screen</Text>
+        </View>
+    )
+}
+
+export default FavoritesScreen
+
+const styles = StyleSheet.create({
+    screen: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
